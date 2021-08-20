7c424abefba3f57898e14049444a0efb2b8b1234
dhruv035 <dhruv035@gmail.com>
2021-08-20 06:07:00 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Button from "@material-ui/core/Button";
+import { ethers } from "ethers";
+import dataidle from "./abi/idledata.json";
+import datadai from "./abi/daidata.json";
 function App() {
+  window.ethereum.enable();
+  //check if Metamask is installed
+
+  async function connection() {
+    const signer = new ethers.providers.Web3Provider(
+      window.ethereum
+    ).getSigner();
+    /*const daiContract = new ethers.Contract(
+      datadai.address,
+      datadai.abi,
+      signer
+    );*/
+    const idleContract = new ethers.Contract(
+      dataidle.address,
+      dataidle.abi,
+      signer
+    );
+    console.log(signer.getAddress());
+    const booler = true;
+    //const tx1 = await daiContract.approve(dataidle.address, 5);
+    //console.log(tx1);
+    const tx = await idleContract.mintIdleToken(
+      ethers.utils.parseEther("4.0"),
+      booler,
+      "0x0000000000000000000000000000000000000000"
+    );
+    console.log(tx);
+  }
+  async function deposit() {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" color="primary" onClick={() => connection()}>
+        Primary
+      </Button>
+      <Button variant="contained" color="primary" onClick={() => deposit()}>
+        Deposit
+      </Button>
     </div>
   );
 }
