d80dcd4c3bb9c17ba28b584b5a417fe733c1830d
dhruv035 <dhruv035@gmail.com>
2021-08-20 11:28:24 +0530

    update 0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import { ethers } from "ethers";
 import dataidle from "./abi/idledata.json";
 import datadai from "./abi/daidata.json";
 import react, { useEffect, useState } from "react";
+import { CardActionArea } from "@material-ui/core";
 const defAdd = "0x0000000000000000000000000000000000000000";
-
+const useStyles = makeStyles({
+  gridClass: {
+    spacing: 0,
+    alignItems: "center",
+    justifyAlign: "center",
+    justifyContent: "center",
+    minHeight: "100vh"
+  },
+  root: {
+    flexGrow: 1,
+    minWidth: 275
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 function App() {
+  const classes = useStyles();
   const [val, setVal] = useState("0.0");
   async function deposit() {
     const signer = new ethers.providers.Web3Provider(
@@ -70,21 +100,51 @@ function App() {
 
   return (
     <div className="App">
-      <Button variant="contained" color="primary" onClick={() => deposit()}>
-        Deposit
-      </Button>
-      <Button variant="contained" color="primary" onClick={() => withdraw()}>
-        Withdraw
-      </Button>
-      <Button variant="contained" color="primary" onClick={() => approve()}>
-        Approve
-      </Button>
-      <TextField
-        id="standard-basic"
-        type="text"
-        label="Standard"
-        onChange={e => handleChange(e.target.value)}
-      ></TextField>
+      <Grid
+        container
+        className={classes.gridClass}
+        direction="column"
+        alignItems="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardContent>
+                <TextField
+                  id="standard-basic"
+                  type="text"
+                  label="Standard"
+                  onChange={e => handleChange(e.target.value)}
+                ></TextField>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => deposit()}
+              >
+                Deposit
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => withdraw()}
+              >
+                Withdraw
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => approve()}
+              >
+                Approve
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
