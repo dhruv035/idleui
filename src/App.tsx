136d5c3296b7516e1613cc5fe7361918643b6ca4
dhruv035 <dhruv035@gmail.com>
2021-08-21 16:41:35 +0530

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,37 @@ import CardContent from "@material-ui/core/CardContent";
 import { ethers } from "ethers";
 import dataidle from "./abi/idledata.json";
 import datadai from "./abi/daidata.json";
+import Paper from "@material-ui/core/Paper";
 import react, { useEffect, useState } from "react";
 import CardActionArea from "@material-ui/core/CardActionArea";
+import daiImage from "./imgofdai.jpg";
+import idaiImage from "./idle.jpeg";
+import { hexZeroPad } from "ethers/lib/utils";
 const defAdd = "0x0000000000000000000000000000000000000000";
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   gridClass: {
     spacing: 0,
     alignItems: "center",
     justifyAlign: "center",
-    justifyContent: "center",
-    minHeight: "100vh"
+    justifyContent: "center"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 1500,
+    maxheight: 1500
   },
   media: {
-    height: 10,
-    paddingTop: "56.25%" // 16:9
+    height: 50,
+    width: 50
+    // 16:9
   },
   root: {
-    flexGrow: 1,
-    minWidth: 275
+    display: "flex"
+  },
+  balanceCard: {
+    display: "flex",
+    flexDirection: "row"
   },
   bullet: {
     display: "inline-block",
@@ -41,7 +54,7 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12
   }
-});
+}));
 function App() {
   const a = new ethers.providers.Web3Provider((window as any).ethereum);
   const daiContract = new ethers.Contract(datadai.address, datadai.abi, a);
@@ -54,6 +67,15 @@ function App() {
   const [bal2, setBal2] = useState("0");
   const [flag, setFlag] = useState(false);
   const [sign, setSign] = useState(b);
+
+  function truncate(str: string, maxDecimalDigits: number) {
+    if (str.includes(".")) {
+      const parts = str.split(".");
+      return parts[0] + "." + parts[1].slice(0, maxDecimalDigits);
+    }
+    return str;
+  }
+
   async function initing() {
     try {
       const provider = new ethers.providers.Web3Provider(
@@ -79,20 +101,37 @@ function App() {
     const daiC = daiContract.connect(sign);
 
     const idleC = idleContract.connect(sign);
-    let txx = await daiContract.balanceOf(sign.getAddress());
-    let txx2 = await idleContract.balanceOf(sign.getAddress());
-    setBal(ethers.utils.formatEther(txx));
-    setBal2(ethers.utils.formatEther(txx2));
+    let txx = await daiC.balanceOf(sign.getAddress());
+    let txx2 = await idleC.balanceOf(sign.getAddress());
+
+    let display1 = ethers.utils.formatEther(txx);
+    setBal(truncate(display1, 4));
+    let display2 = ethers.utils.formatEther(txx2);
+    setBal2(truncate(display2, 4));
   }
   //balancing();
   async function deposit() {
-    const daiC = daiContract.connnect(sign);
-
     const idleC = idleContract.connect(sign);
     console.log(sign.getAddress());
     const booler = true;
-    //const tx1 = await daiContract.approve(dataidle.address, 5);
-    //console.log(tx1);
+
+    const a = new ethers.providers.Web3Provider((window as any).ethereum);
+
+    let filter = {
+      address: dataidle.address,
+      topics: [
+        ethers.utils.id("Transfer(address,address,uint256)"),
+        null,
+        hexZeroPad(await sign.getAddress(), 32)
+      ]
+    };
+
+    a.on(filter, (log, event) => {
+      console.log();
+      balancing();
+      // Emitted any token is sent TO either address
+    });
+
     const tx = await idleC.mintIdleToken(
       ethers.utils.parseEther(val),
       booler,
@@ -109,11 +148,25 @@ function App() {
     );
   }
   async function withdraw() {
+    const a = new ethers.providers.Web3Provider((window as any).ethereum);
     const idleC = idleContract.connect(sign);
-    const abc = await idleContract.redeemIdleToken(
-      ethers.utils.parseEther(val)
-    );
+    const abc = await idleC.redeemIdleToken(ethers.utils.parseEther(val));
     console.log(abc);
+
+    let filter = {
+      address: dataidle.address,
+      topics: [
+        ethers.utils.id("Transfer(address,address,uint256)"),
+        hexZeroPad(await sign.getAddress(), 32),
+        null
+      ]
+    };
+
+    a.on(filter, (log, event) => {
+      console.log();
+      balancing();
+      // Emitted any token is sent TO either address
+    });
   }
   function handleChange(e: string | any) {
     if (e) setVal(e);
@@ -134,76 +187,113 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Grid
-        container
-        className={classes.gridClass}
-        direction="column"
-        alignItems="center"
-        style={{ minHeight: "100vh" }}
-      >
-        <Grid item xs={2}>
-          <Card>
-            <CardMedia className={classes.media} src="logo.svg" />
-          </Card>
-          <Card>
-            <CardContent>
-              <Typography>{bal}</Typography>
-            </CardContent>
-          </Card>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid
+          container
+          spacing={2}
+          className={classes.gridClass}
+          direction="column"
+          alignItems="center"
+        >
+          <Typography variant="h2" component="h5">
+            IDLE
+          </Typography>
+          <Typography variant="h4" component="h5" hidden={!flag}>
+            Logged In
+          </Typography>
         </Grid>
-        <Grid item xs={3}>
-          <Typography>{bal2}</Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Card className={classes.root}>
-            <CardActionArea>
+        <Grid
+          container
+          spacing={2}
+          className={classes.gridClass}
+          direction="row"
+          alignItems="center"
+        >
+          <Grid item xs={5}>
+            <Typography gutterBottom variant="subtitle2" component="h2">
+              Dai Balance
+            </Typography>
+            <Card className={classes.balanceCard}>
+              <CardMedia className={classes.media} image={daiImage}></CardMedia>
               <CardContent>
-                <TextField
-                  id="standard-basic"
-                  type="text"
-                  label="Standard"
-                  onChange={e => handleChange(e.target.value)}
-                ></TextField>
+                <Typography>{bal}</Typography>
               </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => deposit()}
-                disabled={!flag}
-              >
-                Deposit
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => withdraw()}
-                disabled={!flag}
-              >
-                Withdraw
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => approve()}
-                disabled={!flag}
-              >
-                Approve
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => login()}
-                disabled={flag}
-              >
-                MetaMask Login
-              </Button>
-            </CardActions>
-          </Card>
+            </Card>
+          </Grid>
+          <Grid item xs={5}>
+            <Typography gutterBottom variant="subtitle2" component="h2">
+              idleDai Balance
+            </Typography>
+            <Card className={classes.balanceCard}>
+              <CardMedia className={classes.media} image={daiImage}></CardMedia>
+              <CardMedia
+                className={classes.media}
+                image={idaiImage}
+              ></CardMedia>
+              <CardContent>
+                <Typography>{bal2}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+        <Grid
+          container
+          spacing={2}
+          className={classes.gridClass}
+          direction="row"
+          alignItems="center"
+        >
+          <Grid item xs={10}>
+            <Card>
+              <CardActionArea>
+                <CardContent>
+                  <TextField
+                    id="standard-basic"
+                    type="text"
+                    label="Enter Amount"
+                    onChange={e => handleChange(e.target.value)}
+                  ></TextField>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => deposit()}
+                  disabled={!parseFloat(val) || !flag}
+                >
+                  Deposit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => withdraw()}
+                  disabled={!parseFloat(val) || !flag}
+                >
+                  Withdraw
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => approve()}
+                  disabled={!flag}
+                >
+                  Approve
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => login()}
+                  disabled={flag}
+                >
+                  MetaMask Login
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
