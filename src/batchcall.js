02b8901aae815511ce7ae287e89282ab3379fad2
dhruv035 <dhruv035@gmail.com>
2021-09-02 18:53:19 +0530

    batchcall_testing

--- src/batchcall.js
+++ src/batchcall.js
@@ -1,4 +1,5 @@
 import { createWatcher } from "@makerdao/multicall";
+
 export const Watch = async () => {
   const MKR_TOKEN = "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa";
   const MKR_WHALE = "0x037245d2DDcE683436520EFc84590e1F6Fb043fD";
@@ -24,6 +25,27 @@ export const Watch = async () => {
     ],
     config
   );
+
+  watcher.batch().subscribe(updates => {
+    result = updates;
+  });
+
+  await watcher.start();
+  watcher.stop();
+  return result;
+};
+
+export const ProcessQueue = async calls => {
+  const MKR_TOKEN = "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa";
+  const MKR_WHALE = "0x037245d2DDcE683436520EFc84590e1F6Fb043fD";
+  const MKR_FISH = "0xa500B2427458D12Ef70dd7b1E031ef99d1cc09f7";
+
+  const config = {
+    rpcUrl: "https://kovan.infura.io/v3/170121f3e23247b498d8423dfbda782c",
+    multicallAddress: "0x2cc8688c5f75e365aaeeb4ea8d6a480405a48d2a"
+  };
+  let result;
+  const watcher = createWatcher(calls, config);
 
   watcher.batch().subscribe(updates => {
     result = updates;
--- src/batchcall.test.js
+++ src/batchcall.test.js
@@ -1,4 +1,8 @@
-import { Watch } from "./batchcall";
+import { Watch, ProcessQueue } from "./batchcall";
+
+const MKR_TOKEN = "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa";
+const MKR_WHALE = "0x037245d2DDcE683436520EFc84590e1F6Fb043fD";
+const MKR_FISH = "0xa500B2427458D12Ef70dd7b1E031ef99d1cc09f7";
 
 test("check the watcher", () => {
   return Watch().then(result => {
@@ -6,8 +10,30 @@ test("check the watcher", () => {
   });
 });
 
-test("check the add calls feature", () => {
-  return Watch().then(result => {
+test("check the queue feature", () => {
+  const calls = [
+    {
+      target: MKR_TOKEN,
+      call: ["balanceOf(address)(uint256)", MKR_WHALE],
+      returns: [["BALANCE_OF_MKR_WHALE", val => val / 10 ** 18]]
+    },
+    {
+      target: MKR_TOKEN,
+      call: ["balanceOf(address)(uint256)", MKR_FISH],
+      returns: [["BALANCE_OF_MKR_FISH", val => val / 10 ** 18]]
+    }
+  ];
+  return ProcessQueue(calls).then(result => {
     expect(result[0].type).toBe("BALANCE_OF_MKR_WHALE");
   });
+  calls.push({
+    target: MKR_TOKEN,
+    call: ["balanceOf(address)(uint256)", MKR_WHALE],
+    returns: [["BALANCE_OF_MKR_WHALE", val => val / 10 ** 18]]
+  });
+  setTimeout(function() {
+    return ProcessQueue(calls).then(result => {
+      expect(result[2].type).toBe("BALANCE_OF_MKR_WHALE");
+    });
+  }, 3000);
 });
